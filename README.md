975f6b1da7c72ad6d97747d909848f94535e2e57
ManasKhosla <manaskhosla7@gmail.com>
2025-12-31 17:45:19 +0530

    merry updates -- fixed

--- README.md
+++
@@ -1,3 +0,0 @@
-## This is my personal blog
-
-blog live at: https://manaskhosla.in
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767174141',resources: [
+    cacheName: 'chirpy-1767183299',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
